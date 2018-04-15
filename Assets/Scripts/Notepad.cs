71dd38a4db122a05e4960dc4877ecac52d6ab195
Robert Brooks <rabrooks250@gmail.com>
2018-04-15 09:14:51 -0400

    Tested Blender Objests, Materials, and UVs

---
+++ Assets/Scripts/Notepad.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Notepad : MonoBehaviour
+{
+
+	[TextArea]
+	[Tooltip("Doesn't do anything. Just comments shown in inspector")]
+	public string Notes;
+}
